9fadc64852da2b455e196165a633c748b583925a
Jasper Mai <jaspermai25@gmail.com>
2021-01-16 11:19:58 -0800

    added temp code for background.js

--- sketchy/background.js
+++ sketchy/background.js
@@ -1,1 +1,17 @@
 // window.onunload.addListener(())
+
+
+var pageConditions = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      pageUrl: { schemes: ['https','http'] }
+    })
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()]
+}
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([pageConditions]);
+  });
+});
