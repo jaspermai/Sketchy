609813f390ebdadb20cc893aeae355cf56fcd3ba
Terry <terryluan12@gmail.com>
2021-01-17 04:59:03 -0500

    start of google chrome addition

--- sketchy/content.js
+++ sketchy/content.js
@@ -226,3 +226,56 @@ document.addEventListener('DOMContentLoaded', function() {
         png.download = 'note.png';
         })
     })
+
+
+//Google drive stuff
+      var clientId = '573644075529-c4ucnfe2p4u4auoc91069op3p9sd0shn.apps.googleusercontent.com';
+      var apiKey = 'AIzaSyBXWkl9rIf1oSptOPGVc3r5lgeA0vAeAEA';
+      // To enter one or more authentication scopes, refer to the documentation for the API.
+      var scopes = 'https://www.googleapis.com/auth/drive';
+
+      // Use a button to handle authentication the first time.
+      function handleClientLoad() {
+        gapi.client.setApiKey(apiKey);
+        window.setTimeout(checkAuth,1);
+      }
+
+      function checkAuth() {
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: true}, handleAuthResult);
+      }
+
+      function handleAuthResult(authResult) {
+        var authorizeButton = document.getElementById('authorize-button');
+        if (authResult && !authResult.error) {
+          authorizeButton.style.visibility = 'hidden';
+          makeApiCall();
+        } else {
+          authorizeButton.style.visibility = '';
+          authorizeButton.onclick = handleAuthClick;
+        }
+      }
+
+      function handleAuthClick(event) {
+        gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+        return false;
+      }
+
+      // Load the API and make an API call.  Display the results on the screen.
+      function makeApiCall() {
+        gapi.client.load('drive', 'v2', function() {
+
+          var request = gapi.client.drive.files.list ( {'maxResults': 5 } );
+
+          request.execute(function(resp) {          
+            for (i=0; i<resp.items.length; i++) {
+                    var titulo = resp.items[i].title;
+                    var fechaUpd = resp.items[i].modifiedDate;
+                    var userUpd = resp.items[i].lastModifyingUserName;
+
+                    var fileInfo = document.createElement('li');
+                    fileInfo.appendChild(document.createTextNode('TITLE: ' + titulo + ' - LAST MODIF: ' + fechaUpd + ' - BY: ' + userUpd ));                
+                    document.getElementById('content').appendChild(fileInfo);
+            }
+          });        
+        });
+      }
